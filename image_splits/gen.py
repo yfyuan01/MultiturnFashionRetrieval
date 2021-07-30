a67e22fd1fddb488f070302b45f8554bff70f68f
袁一菲 <yyf775@sina.cn>
2021-07-30 12:16:18 +0800

    Delete gen.py

--- image_splits/gen.py
+++
@@ -1,8 +0,0 @@
-with open('../Backup/split.toptee.train.json','rb') as f:
-    l_t_r = json.load(f)
-with open('new.train.toptee1.json','rb') as f:
-    l_t = json.load(f)
-l_t_n = l_t_r
-l_t_n.extend(l_t)
-with open('split.shirt.train.json','wb') as f:
-    json.dump(l_s_n,f)
